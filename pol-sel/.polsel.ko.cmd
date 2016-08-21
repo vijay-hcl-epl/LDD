c875ae3bdc848c155b6244b9edf3c36ee427b96d
VijayGitCode <vijayone1708@gmail.com>
2016-08-21 13:13:14 +0530

    Removed all hidden cmd files

--- pol-sel/.polsel.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/media/sda6/Learning_docs/Dev_Drivers_Docs/Driver_Study/pol-sel/polsel.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /media/sda6/Learning_docs/Dev_Drivers_Docs/Driver_Study/pol-sel/polsel.ko /media/sda6/Learning_docs/Dev_Drivers_Docs/Driver_Study/pol-sel/polsel.o /media/sda6/Learning_docs/Dev_Drivers_Docs/Driver_Study/pol-sel/polsel.mod.o
