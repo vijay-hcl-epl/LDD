cc04d544bb3ef539c1ea63044e2f077fe172c402
VijayGitCode <vijayone1708@gmail.com>
2016-08-21 15:46:09 +0530

    Cleaned up hello proj

--- hello/hello.c
+++ hello/hello.c
@@ -1,6 +1,6 @@
 // #includes
-#include<linux/module.h>
-#include<linux/kernel.h>
+#include<linux/module.h>        // Is it needed?
+#include<linux/kernel.h>        // Is it needed?
 #include<linux/fs.h>		//file operations
 #include<linux/cdev.h>		//used to register the driver
 #include<asm/uaccess.h>		//copy to user;copy from user
@@ -8,14 +8,18 @@
 #define DEVICE_NAME	"hello"
 
 // Device structure
+// Always form your own device structure and embedd cdev
+// This is an example of inheritance in C language where
+// cdev is analogous to base class and hello is analogous to derived class
 struct hello
 {
+   // Below device variable just to track the no. of times
+   // this device is opened,read,write and close
    int numopen;
    int numread;
    int numwrite;
    int numclose;
-   char data[100];
-   char pos;
+   char data[100]; //To store the data
    struct cdev cdev;
 };
 
@@ -27,7 +31,13 @@ int maj_num;
 static int hello_open(struct inode *inode, struct file *filp) {
         struct hello *phlo  =  NULL;
 
+        // Below two statements are needed in almost all char drivers
+
+        // Get the device pointer from inode struct
         phlo = container_of(inode->i_cdev, struct hello, cdev);
+
+        // Store the device pointer in filp->private_data so that this device pointer could be used
+        // in all functions of file operations 
         filp->private_data = phlo;
 
         ++phlo->numopen;
@@ -41,10 +51,18 @@ ssize_t hello_read(struct file *filp, char *buffer, size_t count, loff_t *f_pos)
 
         ++phlo->numread;
         printk("Val of numread is %d\n", phlo->numread);
+
+        // The driver can read the value filp->f_pos but should not normally change it
         printk("Val of fpos is %d\n", (int)filp->f_pos);
         printk("Val of count is %d\n", (int)count);
         
+        // Remember filp are specific to process that opens this
+        // so filp->f_pos stores the last accessed index
         copy_to_user(buffer, &phlo->data[filp->f_pos], count);
+
+        // Always increment the *f_pos so that kernel remembers it
+        // read and write should update the position using *f_pos
+        // Kernel later uses this value and updates the value filp->f_pos
         *f_pos += count;
 
 	return 0;
@@ -55,10 +73,18 @@ ssize_t hello_write(struct file *filp, const char *buffer, size_t count, loff_t 
 
         ++phlo->numwrite;
         printk("Val of numwrite is %d\n", phlo->numwrite);
+
+        // The driver can read the value filp->f_pos but should not normally change it
         printk("Val of fpos is %d\n", (int)filp->f_pos);
         printk("Val of count is %d\n", (int)count);
 
+        // Remember filp are specific to process that opens this
+        // so filp->f_pos stores the last accessed index
         copy_from_user(&phlo->data[filp->f_pos], buffer, count);
+
+        // Always increment the *f_pos so that kernel remembers it
+        // read and write should update the position using *f_pos
+        // Kernel later uses this value and updates the value filp->f_pos
         *f_pos += count;
 
 	return 0;
@@ -75,6 +101,7 @@ int hello_close(struct inode *inode, struct file *filp) {
 }
 
 // File Operations struct
+// Declare after fops function definitions
 struct file_operations fops = {
         .owner   = THIS_MODULE,
         .read    = hello_read,
@@ -98,12 +125,16 @@ static int hello_init(void) {
 	printk("Use mknod for device file\n");
 
 	//pcdev = cdev_alloc();
+
+        // Initialise the cdev field inside your structure
         cdev_init(&hlo.cdev, &fops);
         hlo.cdev.owner = THIS_MODULE;
         printk("The addr of cdev is %x\n", (unsigned int)&hlo.cdev);
 
 	/* Now we created a cdev, we have to add it to the kernel
 	int cdev_add(struct cdev *dev, dev_t num, unsigned int count) */
+        // You should not call cdev_add until your driver is completely 
+        // ready to handle operations on the device.
 	ret = cdev_add(&hlo.cdev, dev_num, 1);
 	if(ret < 0) {
 		printk("Helloworld : Unable to add cdev to kernel\n");
@@ -114,6 +145,7 @@ static int hello_init(void) {
 
 static void hello_exit(void) {
 
+        // To remove a char device from the system
 	cdev_del(&hlo.cdev);
 	
 	unregister_chrdev_region(dev_num, 1);
